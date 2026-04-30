263971206be44a13992d3d23a8638d139663c0aa
checd <checdd@gmail.com>
2026-04-30 14:03:54 +0200

    Adding while loops in the main function instead of nested loops for user and admin choices

--- main.cpp
+++ main.cpp
@@ -20,44 +20,51 @@ int main(){
         switch (userChoice){
             case 1 : 
                 userinterface.userLogin();   
-                userinterface.displayUserOperations();
-                userChoice = maininterface.validateUserInput();
-                if (userChoice == 0){
-                    cout << "Cannot exit while logged in. Please choose a valid operation.\n";
-                }
-                else if (userChoice == 1){
-                    userinterface.listAllFloors();
-                }
-                else if (userChoice == 2){
-                    userinterface.showInfoAboutAccount();
-                }
-                else { 
-                    maininterface.printLogOut();
+                
+                while(true){
+                    userinterface.displayUserOperations();
+                    userChoice = maininterface.validateUserInput();
+
+                    if (userChoice == 1){
+                        userinterface.listAllFloors();
+                    }
+                    else if (userChoice == 2){
+                        userinterface.showInfoAboutAccount();
+                    }
+                    else if (userChoice == 3){
+                        maininterface.printLogOut();
+                        break; // exit to main menu
+                    }
+                    else  {
+                        cout << "Invalid choice\n";
+                    }
                 }
                 break;
             case 2 :
                 admininterface.adminLogin();
-                admininterface.displayAdminOperations();
-                userChoice = maininterface.validateUserInput();
-                if (userChoice == 0){
-                    cout << "Cannot exit while logged in. Please choose a valid operation.\n";
+                
+                while(true){
+                    admininterface.displayAdminOperations();
+                    userChoice = maininterface.validateUserInput();
+
+                    if (userChoice == 1){
+                        admininterface.chooseFloor();
+                    }
+                    else if (userChoice == 2){
+                        admininterface.listAllUsersView();
+                    }
+                    else if (userChoice == 3){
+                        admininterface.createNewUser();
+                    }
+                    else if (userChoice == 4){
+                        maininterface.printLogOut();
+                        break; // exit to main menu
+                    }
+                    else  {
+                        cout << "Invalid choice\n";
+                    }
                 }
-                else if (userChoice == 1){
-                    admininterface.chooseFloor();
-                }
-                else if (userChoice == 2){
-                    admininterface.listAllUsersView();
-                }
-                else if (userChoice == 3){
-                    admininterface.createNewUser();
-                }
-                else if (userChoice == 4){ 
-                    maininterface.printLogOut();
-                }
-                else {
-                    cout << "Invalid choice\n";
-                }
-                break;
+                    break;
             case 0 :
                 maininterface.printExit();
                 break;
